014b930136177535217c28f53ed6955e8e10efc5
Peifeng Qiu <pqiu@pivotal.io>
2018-09-11 22:07:25 +0900

    cpp style output, multi servers

--- client.cc
+++ client.cc
@@ -8,13 +8,21 @@
 #include <librdkafka/rdkafka.h>
 #include <thread>
 #include <vector>
-#include <stdio.h>
+#include <atomic>
+#include <iostream>
+#include <sstream>
 
 using namespace KafkaConsumerServer;
 using namespace grpc;
+using std::cout;
+using std::endl;
 
 const char brokers[] = "127.0.0.1:9092";
 const char topic[] = "test";
+const int channels = 4;
+const int basePort = 6000;
+std::unique_ptr<Kafka::Stub> stubs[channels];
+std::atomic<size_t> messages = 0;
 int main(int argc, char **argv)
 {
 	if (argc < 2)
@@ -26,39 +34,47 @@ int main(int argc, char **argv)
 	rd_kafka_resp_err_t err = rd_kafka_metadata(k, false, t, &meta, 2000);
 	if (err != RD_KAFKA_RESP_ERR_NO_ERROR)
 	{
-		printf("Failed to get metadata: %s\n", rd_kafka_err2str(err));
+		cout << "Failed to get metadata: " << rd_kafka_err2str(err) << endl;
 		return 1;
 	}
 
 	int count = meta->topics->partition_cnt;
-	printf("%d partitions\n", count);
+	cout << count << " partitions" << endl;
 
 	rd_kafka_metadata_destroy(meta);
 	rd_kafka_topic_destroy(t);
 	rd_kafka_destroy(k);
 
-	auto ch = CreateChannel(argv[1], InsecureChannelCredentials());
-	auto stub = Kafka::NewStub(ch);
+	for (int i = 0; i < channels; i++)
+	{
+		std::stringstream address;
+		address << "127.0.0.1:";
+		address << basePort + i;
+		auto ch = CreateChannel(address.str(), InsecureChannelCredentials());
+		stubs[i] = Kafka::NewStub(ch);
+	}
 
 	std::vector<std::thread> threads;
+
 	for (int i = 0; i < count; i++)
 	{
-		threads.emplace_back([&]() {
+		threads.emplace_back([](int part) {
 			ConsumerJob job;
 			job.set_brokers(brokers);
 			job.set_topic(topic);
 			job.set_batchsize(10);
 			job.set_batchinterval(500);
-			job.set_partitionid(i);
+			job.set_partitionid(part);
 			job.set_offset(0);
 
+			auto& stub = stubs[part % channels];
 			JobID id;
 			{
 				ClientContext ctx;
 				Status s = stub->AddJob(&ctx, job, &id);
 				if (!s.ok())
 				{
-					printf("AddJob failed\n");
+					cout << "AddJob failed" << endl;
 					return;
 				}
 			}
@@ -80,7 +96,7 @@ int main(int argc, char **argv)
 					Status s = reader->Finish();
 					if (!s.ok())
 					{
-						printf("ReadBatch failed\n");
+						cout << "ReadBatch failed" << endl;
 						return;
 					}
 				}
@@ -91,14 +107,14 @@ int main(int argc, char **argv)
 					Status s = stub->GetBatchInfo(&ctx, id, &info);
 					if (!s.ok())
 					{
-						printf("GetBatchInfo failed\n");
+						cout << "GetBatchInfo failed" << endl;
 						return;
 					}
 					if (info.eof())
 						break;
 				}
 			}
-			printf("Partition %d total %d\n", i, total);
+			cout << "Partition " << part << " total " << total << endl;
 
 			{
 				Empty empty;
@@ -106,14 +122,16 @@ int main(int argc, char **argv)
 				Status s = stub->DeleteJob(&ctx, id, &empty);
 				if (!s.ok())
 				{
-					printf("DeleteJob failed\n");
+					cout << "DeleteJob failed" << endl;
 					return;
 				}
 			}
-		});
+			messages += total;
+		}, i);
 	}
 	for (auto it = threads.begin(); it != threads.end(); it++)
 		it->join();
 
+	cout << "Total messages: " << messages << endl;
 	return 0;
 }
